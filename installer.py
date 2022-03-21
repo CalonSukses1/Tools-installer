8bbabb109c0da9278234af6f93479b77aeaf7c52
MR.D3XSTER <81459706+CalonSukses1@users.noreply.github.com>
2022-03-21 17:32:07 +0800

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,38 @@
+#GwGakRecodePunyaOrngAnjing
+#1/bin/python
+
+import os
+import sys
+import time
+
+def clear():
+    os.system("clear")
+    os.system("figlet Ninym")
+
+def menu():
+    clear()
+    print "\tTools by \33[31;1mRen7\33[0;00m\n"
+    print "\tgw nggak recode ya kontl"
+    print "\n1.install nmap"
+    print "\n2.install wafw00f"
+    print "\n3.install python"
+    print "\n4.install python2"
+    print "\n5.install debian" 
+    pil = raw_input("\npilih : ")
+    if pil =="1":
+       os.system("pkg install nmap")
+    elif pil =="2":
+       os.system("pip install wafw00f")
+    elif pil =="3":
+       os.system("pkg install python")
+    elif pil =="4":
+       os.system("pkg install python2")
+    elif pil =="5":
+       os.system("pkg update && pkg upgrade -y")
+       os.system("pkg install proot proot-distro")
+       os.system("proot-distro install debian")
+       os.system("proot-distro login debian")
+    else:
+      print "\33unknown input\33[0;00m"
+
+menu()
